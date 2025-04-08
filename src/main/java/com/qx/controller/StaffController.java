f7bd00d5582244c4a82bb15505229343c0fd86f7
qixia <2842939523@qq.com>
2025-04-08 19:25:31 +0200

    添加删除员工功能

--- src/main/java/com/qx/controller/StaffController.java
+++ src/main/java/com/qx/controller/StaffController.java
@@ -17,10 +17,14 @@ public class StaffController {
     @Resource
     StaffMapper staffMapper;
 
-    @GetMapping("getAll")
+    @GetMapping("/getAll")
     public List<Staff> getAll(Staff staff) {
-
         return staffMapper.selectList(new QueryWrapper<>(staff));
+    }
+
+    @PostMapping("/deleteStaff")
+    public boolean deleteStaff(@RequestBody Staff staff) {
+        return staffMapper.deleteById(staff.getId()) > 0;
     }
 
 }
